23df3f16039efff145afb4a0dd850d57503d10bb
aekald <aekald@taltech.ee>
2024-02-08 16:57:43 +0200

    client

--- game/core/src/ee/taltech/pony_dash_for_spikes_salvation/pony_dash_for_spikes_salvation_game.java
+++ game/core/src/ee/taltech/pony_dash_for_spikes_salvation/pony_dash_for_spikes_salvation_game.java
@@ -1,30 +1,73 @@
 package ee.taltech.pony_dash_for_spikes_salvation;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.esotericsoftware.kryonet.Client;
+
+import java.io.IOException;
 
 public class pony_dash_for_spikes_salvation_game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+	private int x = 0, y = 0;
+
+	private Client client;
+
 	@Override
 	public void create () {
+		client = new Client();
+		client.start();
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		client.sendTCP("Start");
+		try {
+			client.connect(5000, "localhost", 8080, 8081);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void sendPositionInfoToServer() {
+		client.sendUDP(x + "|" + y);
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			x -= 10;
+			sendPositionInfoToServer();
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			x += 10;
+			sendPositionInfoToServer();
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			y += 10;
+			sendPositionInfoToServer();
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			y -= 10;
+			sendPositionInfoToServer();
+		}
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
+		client.close();
+		try {
+			client.dispose();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 		batch.dispose();
 		img.dispose();
 	}
