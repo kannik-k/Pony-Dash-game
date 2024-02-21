9fb2dc6cba7f2de6f91743f7584f2ae072c5d005
Karoliina <kannik@taltech.ee>
2024-02-21 13:20:03 +0200

    Mängija keha objekt on loodud

--- game/core/src/ee/taltech/pony_dash_for_spikes_salvation/screens/PlayScreen.java
+++ game/core/src/ee/taltech/pony_dash_for_spikes_salvation/screens/PlayScreen.java
@@ -1,17 +1,23 @@
 package ee.taltech.pony_dash_for_spikes_salvation.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import ee.taltech.pony_dash_for_spikes_salvation.Main;
+import ee.taltech.pony_dash_for_spikes_salvation.sprites.PonySprite;
 
 public class PlayScreen implements Screen {
     private final Main game;
@@ -26,6 +32,8 @@ public class PlayScreen implements Screen {
 
     // Box2d muutujad
     private World world;
+    private Box2DDebugRenderer b2dr;
+    private PonySprite player;
 
     public PlayScreen(Main game){
         this.game = game;
@@ -39,9 +47,60 @@ public class PlayScreen implements Screen {
         renderer = new OrthogonalTiledMapRenderer(map,1);
 
         gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+        world = new World(new Vector2(0, -10), true);
+        b2dr = new Box2DDebugRenderer();
+
+        player = new PonySprite(this.getWorld());
+
+        // Ajutine, tuleb hiljem ümber tõsta
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+
+        // Ground Layer 8
+        for (MapObject object : map.getLayers().get(8).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+            body = world.createBody(bdef);
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        // Platform Layer 10
+        for (MapObject object : map.getLayers().get(10).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+            body = world.createBody(bdef);
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+    }
+
+    public  void hanelInput(float dt) {
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)){
+            player.b2body.applyLinearImpulse(new Vector2(0, 4f), player.b2body.getWorldCenter(), true);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player.b2body.getLinearVelocity().x <= 2) {
+            player.b2body.applyLinearImpulse(new Vector2(25f, 0), player.b2body.getWorldCenter(), true);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && player.b2body.getLinearVelocity().x >= -2) {
+            player.b2body.applyLinearImpulse(new Vector2(-25f, 0), player.b2body.getWorldCenter(), true);
+        }
     }
 
     public void update(float dt) {
+        hanelInput(dt);
+        gameCam.position.x = player.b2body.getPosition().x;
+        gameCam.position.y = player.b2body.getPosition().y;
+        world.step(1/60f, 6, 2);
         gameCam.update();
         renderer.setView(gameCam);
     }
@@ -56,6 +115,7 @@ public class PlayScreen implements Screen {
         Gdx.gl.glClearColor((float)0.941, (float)0.698, (float)0.784, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         renderer.render();
+        b2dr.render(world, gameCam.combined);
         game.batch.setProjectionMatrix(gameCam.combined); // Renderdab pildi kaameraga kaasa
         game.batch.begin(); // Opens window
         game.makeAllPlayersMove();
---
+++ game/core/src/ee/taltech/pony_dash_for_spikes_salvation/sprites/PonySprite.java
@@ -0,0 +1,32 @@
+package ee.taltech.pony_dash_for_spikes_salvation.sprites;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class PonySprite extends Sprite {
+    public World world;
+    public Body b2body;
+
+    public PonySprite(World world) {
+        this.world = world;
+        definePony();
+    }
+
+    public void definePony() {
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(32, 80);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        b2body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(8);
+
+        fdef.shape = shape;
+        b2body.createFixture(fdef);
+    }
+}
