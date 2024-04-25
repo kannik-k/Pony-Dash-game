be68e6ec5eeda203f40436ce56785716d785ecdc
Faina <fadoms@taltech.ee>
2024-04-25 20:25:06 +0300

    walls

---
+++ game/assets/Tiled assets/Sign.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Sign" tilewidth="16" tileheight="16" tilecount="9" columns="3">
+ <image source="../../../Game Assets/03.png" width="48" height="48"/>
+</tileset>
